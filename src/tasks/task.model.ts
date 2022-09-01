04eaa146d0e2d349f6f19bcec60e5bb7b35efd5b
Hector Garzon <hferney@technisys.com>
2022-09-01 18:15:36 -0500

    Feature: Creating a Task

--- src/tasks/task.model.ts
+++ src/tasks/task.model.ts
@@ -5,8 +5,10 @@ export interface Task {
   status: TaskStatus;
 }
 
-enum TaskStatus {
-  OPEN,
-  IN_PROGRESS,
-  DONE,
+export type CreateTask = Pick<Task, 'title' | 'description'>;
+
+export enum TaskStatus {
+  OPEN = 'OPEN',
+  IN_PROGRESS = 'IN_PROGRESS',
+  DONE = 'DONE',
 }
